6502f97879c073acb35821c327cd7534a3dd6545
Renaud Paquay <rpaquay@chromium.org>
2014-12-08 11:07:35 -0800

    Initial repo: carbon copy of axiom_thing.
    https://github.com/rginda/axiom_thing/commit/11bd42c55f86c284af3b211bb52fd90174174a9f

---
+++ README.md
@@ -0,0 +1,1 @@
+A quick and dirty axiom plugin example.
---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+module.exports = function(grunt) {
+  // Load the grunt related dev deps listed in package.json.
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Read in our own package file.
+  var pkg = grunt.file.readJSON('./package.json');
+
+  grunt.initConfig({
+    pkg: pkg,
+    env: process.env,
+
+    clean: {
+      all: ['out'],
+      transpile: ['out/amd']
+    },
+
+    // Convert our ES6 import/export keywords into plain js.  We generate an
+    // AMD version for use in the browser, and a CommonJS version for use in
+    // node.js.
+    transpile: {
+      amd: {
+        type: "amd",
+        files: [{
+          expand: true,
+          cwd: 'lib/',
+          src: ['**/*.js'],
+          dest: 'out/amd/lib/'
+        }]
+      }
+    },
+
+    copy: {
+      html_out: {
+        files: [{
+          expand: true,
+          cwd: 'html',
+          src: ['*.html'],
+          dest: 'out/'
+        },
+      ]}
+    },
+
+    // Linting.
+    jshint: {
+      lib: {
+        src: ['lib/**/*.js'],
+        options: {
+          jshintrc: '.jshintrc',
+          force: false,
+          verbose: true
+        }
+      },
+    },
+  });
+
+  grunt.registerTask('build', ['jshint', 'clean:transpile', 'transpile',
+                               'copy']);
+  grunt.registerTask('default', ['build']);
+};
