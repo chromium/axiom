935bb29bd5116a08a476592b49214d312d871244
Renaud Paquay <rpaquay@chromium.org>
2014-12-16 13:39:34 -0800

    Fix running python by setting PYTHONHOME to '/'.

--- src/addon/pnacl/lib/axiom_pnacl/executables.js
+++ src/addon/pnacl/lib/axiom_pnacl/executables.js
@@ -10,8 +10,8 @@ import PnaclCommand from 'axiom_pnacl/pnacl_command';
 
 // @note ExecuteContext from 'axiom/bindings/fs/execute_context'
 
-var createCommand = function(commandName, sourceUrl, tarFilename) {
-  var command = new PnaclCommand(commandName, sourceUrl, tarFilename);
+var createCommand = function(name, sourceUrl, opt_tarFilename, opt_env) {
+  var command = new PnaclCommand(name, sourceUrl, opt_tarFilename, opt_env);
   return command.run.bind(command);
 };
 
@@ -20,7 +20,15 @@ export var executables = function(sourceUrl) {
     'curl(@)': createCommand('curl', sourceUrl),
     'nano(@)': createCommand('nano', sourceUrl, 'nano.tar'),
     'nethack(@)': createCommand('nethack', sourceUrl, 'nethack.tar'),
-    'python(@)': createCommand('python', sourceUrl, 'pydata_pnacl.tar'),
+    'python(@)':
+        // Note: We set PYTHONHOME to '/' so that the python library files
+        // are loaded from '/lib/python2.7', which is the prefix path used in
+        // 'pydata_pnacl.tar'. By default, python set PYTHONHOME to be the
+        // prefix of the executable from arg[0]. This conflicts with the way
+        // we set arg[0] to tell pnacl to where to load the .tar file from.
+        createCommand('python', sourceUrl, 'pydata_pnacl.tar', {
+          '$PYTHONHOME': '/'
+        }),
     'unzip(@)': createCommand('unzip', sourceUrl),
     'vim(@)': createCommand('vim', sourceUrl, 'vim.tar'),
   };
