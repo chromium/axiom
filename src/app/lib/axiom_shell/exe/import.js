74128a0d7b4c7a1f980da335cb2ef522fa51862d
Robert Ginda <rginda@chromium.org>
2014-12-16 15:52:38 -0800

    Modify import command to take urls from arg._
    Modify import to tak the url-to-import from the loose command
    line args.  Also modify it to accept multiple urls in a single
    command.

--- src/app/lib/axiom_shell/exe/import.js
+++ src/app/lib/axiom_shell/exe/import.js
@@ -7,7 +7,7 @@ import environment from 'axiom_shell/environment';
 
 export var main = function(cx) {
   var onImportLoaded = function(resolve, reject, e) {
-    var todoList = [];
+    var pluginList = [];
 
     // There could be multiple x-axiom-plugin nodes in this import.
     var plugins = e.target.import.querySelectorAll('x-axiom-plugin');
@@ -25,38 +25,38 @@ export var main = function(cx) {
         console.stderr('Missing x-main-module');
 
       if (desc && main)
-        todoList.push([desc, main]);
+        pluginList.push([desc, main]);
     }
 
     // Bail out if we don't find any valid plugins.
-    if (!todoList.length)
+    if (!pluginList.length)
       return reject(null);
 
-    var nextTodo = function() {
-      if (!todoList.length)
-        return resolve(null);
+    var nextPlugin = function() {
+      if (!pluginList.length)
+        return nextImportUrl(resolve, reject);
 
-      var todo = todoList.shift();
-      var descriptor = environment.requireModule(todo[0])['default'];
+      var plugin = pluginList.shift();
+      var descriptor = environment.requireModule(plugin[0])['default'];
       if (!descriptor) {
-        return resolve(new AxiomError.Runtime(
-            'No descriptor exported by: ' + todo[0]));
+        return reject(new AxiomError.Runtime(
+            'No descriptor exported by: ' + plugin[0]));
       }
 
       var module = environment.defineModule(descriptor);
       cx.stdout('New module: ' + module.moduleId + '\n');
 
-      var main = environment.requireModule(todo[1])['default'];
+      var main = environment.requireModule(plugin[1])['default'];
       if (!main) {
-        return resolve(new AxiomError.Runtime(
-            'No main exported by: ' + todo[1]));
+        return reject(new AxiomError.Runtime(
+            'No main exported by: ' + plugin[1]));
       }
 
       try {
         return main(module, { sourceUrl: e.target.import.URL }).then(
             function() {
               cx.stdout('Module ready.\n');
-              return nextTodo();
+              return nextPlugin();
             }).catch(function(err) {
               return reject(err);
             });
@@ -65,29 +65,40 @@ export var main = function(cx) {
       }
     };
 
-    return nextTodo();
+    return nextPlugin();
+  };
+
+  var nextImportUrl = function(resolve, reject) {
+    if (!cx.arg._.length)
+      resolve(null);
+
+    var url = cx.arg._.shift();
+    if (!url)
+      return reject(new AxiomError.Missing('url'));
+
+    var link = document.createElement('link');
+    link.rel = 'import';
+    link.href = url;
+    link.addEventListener('error', function(e) {
+        cx.stdout('Error loading import: ' + e.target.href + '\n');
+        link.parentNode.removeChild(link);
+        return reject(null);
+      });
+
+    link.addEventListener('load',
+                          onImportLoaded.bind(null, resolve, reject));
+
+    document.head.appendChild(link);
   };
 
   return new Promise(function(resolve, reject) {
-      cx.ready();
-      var url = cx.arg.url;
-      if (!url)
-        return reject(new AxiomError.Missing('url'));
+    cx.ready();
 
-      var link = document.createElement('link');
-      link.rel = 'import';
-      link.href = url;
-      link.addEventListener('error', function(e) {
-          cx.stdout('Error loading import: ' + e.target.href + '\n');
-          link.parentNode.removeChild(link);
-          return reject(null);
-        });
+    if (!cx.arg._.length)
+      return reject(new AxiomError.Missing('url'));
 
-      link.addEventListener('load',
-                            onImportLoaded.bind(null, resolve, reject));
-
-      document.head.appendChild(link);
-    });
+    nextImportUrl(resolve, reject);
+  });
 };
 
 export default main;
