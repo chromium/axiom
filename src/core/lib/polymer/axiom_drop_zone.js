103350c8077bb9eee83c8bd94edd02cf91b86174
Renaud Paquay <rpaquay@chromium.org>
2014-12-18 14:40:56 -0800

    Fix CSS to better support drag-drop.
    Add debugging code for events.

--- src/core/lib/polymer/axiom_drop_zone.js
+++ src/core/lib/polymer/axiom_drop_zone.js
@@ -3,7 +3,43 @@
 // found in the LICENSE file.
 
 Polymer('axiom-drop-zone', {
+  ready: function() {
+    function getPathString(event) {
+      var s = "";
+      s = event.path[0].tagName;
+      for(var i = 1; i < event.path.length; i++) {
+        var elem = event.path[i];
+        //if (elem.tagName && elem.tagName.substr(0, 5) === "AXIOM") {
+          if (s !== "")
+            s += ", "
+          s += elem.tagName;
+        //}
+      }
+      return '[' +  s + ']';
+    }
+    var lastDragOver = null;
+    this.addEventListener('dragenter', function(e) {
+      console.log("dragenter: " + getPathString(e), this);
+      e.target.setAttribute("highlight", "");
+      lastDragOver = null;
+      e.preventDefault();
+    });
+    this.addEventListener('dragleave', function(e) {
+      console.log("dragleave: " + getPathString(e), this);
+      e.target.removeAttribute("highlight");
+      lastDragOver = null;
+    });
+    this.addEventListener('dragover', function(e) {
+      if (lastDragOver === this)
+        return;
+      console.log("dragover: " + getPathString(e), this);
+      lastDragOver = this;
+
+      //e.preventDefault();
+    });
+  },
   computed: {
-    arrowtype: '(position == "top" || position == "right") ? "up" : "down"'
-  }
+    arrowtype: '(position == "top" || position == "right") ? "up" : "down"',
+    orientation: '(position == "top" || position == "bottom") ? "horizontal" : "vertical"',
+  },
 });
