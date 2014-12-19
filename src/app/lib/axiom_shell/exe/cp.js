ec6d2851356e1e644679933f2586ecd8b83d5fa5
Gaurav Agarwal <grv@grv-macbookpro.roam.corp.google.com>
2014-12-19 12:57:10 -0800

    Make cp parse arguments directly.

--- src/app/lib/axiom_shell/exe/cp.js
+++ src/app/lib/axiom_shell/exe/cp.js
@@ -18,13 +18,19 @@ import Path from 'axiom/fs/path';
 import environment from 'axiom_shell/environment';
 import util from 'axiom_shell/util';
 
+var CP_CMD_USAGE_STRING = 'usage: cp sourceFile targetFile';
+
 export var main = function(executeContext) {
   executeContext.ready();
 
   var arg = executeContext.arg;
+  if (!arg._ || (arg._.length != 2) || arg.h || arg.help) {
+    executeContext.stdout(CP_CMD_USAGE_STRING + '\n');
+    return Promise.resolve(null);
+  }
 
-  var fromPathSpec = arg.src;
-  var toPathSpec = arg.dst;
+  var fromPathSpec = arg._[0];
+  var toPathSpec = arg._[1];
   fromPathSpec = Path.abs(executeContext.getEnv('$PWD', '/'), fromPathSpec);
   toPathSpec = Path.abs(executeContext.getEnv('$PWD', '/'), toPathSpec);
 
