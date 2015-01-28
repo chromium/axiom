7d59b53a21381f7f81a5d3980d29c57035856619
Gaurav Agarwal <grv@grv-macbookpro.roam.corp.google.com>
2015-01-27 18:23:28 -0800

    Create a fake window to load polymer.

--- src/app/boot/chrome_app_background.js
+++ src/app/boot/chrome_app_background.js
@@ -12,4 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-__axiomRequire__('shell/chrome/background');
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('html/main_window.html', {
+    'bounds': {
+      'width': 2,
+      'height': 2
+    }
+  });
+});
