721391cee8ddbe47c5d819cff4897f8c058507cf
ussuri <sg.shevchenko@gmail.com>
2015-03-09 15:15:41 -0700

    Fix Closure warnings

--- gruntfile.js
+++ gruntfile.js
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
         cwd: 'lib/',
         js: ['**/*.js',
              '../third_party/closure-compiler/contrib/externs/jasmine.js',
+             '../third_party/google_api/externs/google_api.js',
              '../tmp/third_party/dcodeIO/fs.js',
              '../tmp/third_party/dcodeIO/buffer.js',
              '../tmp/third_party/dcodeIO/stream.js',
